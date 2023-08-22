4c9e636d794bac0ce0ac154ad28d055edcc095bb
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-08-22 11:53:19 +0300

    feat: create the Navbar and NavbarItem components

---
+++ components/NavbarItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+const NavbarItem = ({ title, param }) => {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
+  return (
+    <div>
+      <Link
+        className={`m-4 text-[#f5d7db] font-semibold p-2 ${
+          genre &&
+          genre === param &&
+          "underline underline-offset-8 decoration-4  decoration-amber-500 rounded-xl"
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
+    </div>
+  );
+};
+
+export default NavbarItem;
