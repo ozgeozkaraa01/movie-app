531f88884839066e7b433512683f3aa69a6e4aef
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-08-22 11:59:44 +0300

    feat: create Header and MenuItem components

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Head from "next/head";
+import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-zinc-100">
       <Head>
         <title>Next Movie App</title>
         <meta name="description" content="created by Özgenur Özkara" />
         <link rel="icon" href="/assets/favicon-32x32.png" />
       </Head>
+      <Header />
       <Navbar />
     </div>
   );
